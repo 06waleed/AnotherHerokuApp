b36b61e0c1b5c2322d21831e737775b000c2a79a
06waleed <waleed_afzal@hotmail.com>
2016-04-05 02:20:34 +0500

    ReadMe

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyFirstApp</title>
+</head>
+<body>
+<h1 style="text-align: center;">Hello from the other side...</h1>
+<br/>
+<br/>
+<br/>
+<h1 style="text-align: center;">My First Heroku App</h1>
+</body>
+</html>
